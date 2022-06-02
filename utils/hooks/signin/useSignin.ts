ec6aecd36b0d11ff94cf8569788744f3c7f54a1e
Jeon gyuhyun <jgh10070@naver.com>
2022-06-03 08:43:50 +0900

    [UPDATE] useSignin hooks

---
+++ utils/hooks/signin/useSignin.ts
@@ -0,0 +1,39 @@
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { useSelectState } from '../common';
+import {
+  refreshToken,
+  setEmail,
+  setPassword,
+  signin,
+} from '@/../core/redux/signin';
+import { IS_LOGIN } from '@/../core/redux/auth';
+import { signinRequest } from '@/../constance/types';
+
+const useSignin = () => {
+  const dispatch = useDispatch();
+  const signinState = useSelectState().signin;
+  useEffect(() => {
+    if (signinState.error.type) dispatch({ type: IS_LOGIN, payload: false });
+  }, [signinState.error]);
+  const setState = {
+    setId: (payload: string) => {
+      dispatch(setEmail(payload));
+    },
+    setPassword: (payload: string) => {
+      dispatch(setPassword(payload));
+    },
+    signin: (payload: signinRequest) => {
+      dispatch(signin(payload));
+    },
+    refreshToken: (callback: () => void) => {
+      dispatch(refreshToken({ callback }));
+    },
+  };
+  return {
+    state: signinState,
+    setState,
+  };
+};
+
+export default useSignin;
