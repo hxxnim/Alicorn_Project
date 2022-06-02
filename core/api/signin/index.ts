c23f936d991db58c3f1b01351599a373e4aba8ef
Jeon gyuhyun <jgh10070@naver.com>
2022-06-03 08:54:13 +0900

    [UPDATE] signin api

---
+++ core/api/signin/index.ts
@@ -0,0 +1,35 @@
+import uri from '../../../constance/uri';
+import { signinRequest } from '@/../constance/types';
+import { refreshResponse, signinResponse } from '@/../constance/types'
+import { getRequest } from '../default';
+
+export const signinApi = async (body: signinRequest) => {
+  try {
+    const request = getRequest();
+    const response = await request.post<signinResponse>(uri.signin, body);
+    localStorage.setItem('access_token', response.data.access_token);
+    localStorage.setItem('refresh_token', response.data.refresh_token);
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const refreshTokenApi = async () => {
+  try {
+    const request = getRequest();
+    const { data } = await request.put<refreshResponse>(
+      uri.signin,
+      {},
+      {
+        headers: {
+          'x-refresh-token': localStorage.getItem('refresh_token'),
+        },
+      },
+    );
+    localStorage.setItem('access_token', data.access_token);
+    return data;
+  } catch (error) {
+    throw error;
+  }
+};
