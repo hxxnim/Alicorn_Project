cf8271d6af8e64fef2958f9eec20bfa250e772d6
Jeon gyuhyun <jgh10070@naver.com>
2022-06-02 16:22:42 +0900

    [SET] styles directory

---
+++ alicorn_project/styles/theme.ts
@@ -0,0 +1,14 @@
+export const COLOR = { 
+    color_container_border: "#E0E0E0",
+    color_blue_strong: "#5B79FF",
+    color_blue_weak: "#95A9FF",
+    color_contact_user_hover: "#4e61ff",
+    color_neon_blue: "#17DFD0",
+    color_selected_background: "#EFF2F7",
+    color_default_background: "#FFFFFF",
+    color_message_border: "#ECEEF4",
+    color_message_text: "#AAAAAA",
+    color_message_head_text: "#2E3B4A",
+    color_input: "#B4B4B4",
+    color_scroll: "#A6A6A6",
+}
