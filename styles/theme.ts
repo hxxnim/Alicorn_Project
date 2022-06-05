d20cb286b4fb45d2b7958f772ac89ab1607c23e2
Jeon gyuhyun <jgh10070@naver.com>
2022-06-06 02:50:30 +0900

    [UPDATE] add another color property

--- styles/theme.ts
+++ styles/theme.ts
@@ -1,14 +1,26 @@
 export const COLOR = { 
-    color_container_border: "#E0E0E0",
-    color_blue_strong: "#5B79FF",
-    color_blue_weak: "#95A9FF",
-    color_contact_user_hover: "#4e61ff",
-    color_neon_blue: "#17DFD0",
-    color_selected_background: "#EFF2F7",
-    color_default_background: "#FFFFFF",
-    color_message_border: "#ECEEF4",
-    color_message_text: "#AAAAAA",
-    color_message_head_text: "#2E3B4A",
-    color_input: "#B4B4B4",
-    color_scroll: "#A6A6A6",
+  color_container_border: "#E0E0E0",
+  color_blue_strong: "#5B79FF",
+  color_blue_weak: "#95A9FF",
+  color_contact_user_hover: "#4e61ff",
+  color_neon_blue: "#17DFD0",
+  color_selected_background: "#EFF2F7",
+  color_default_background: "#FFFFFF",
+  color_message_border: "#ECEEF4",
+  color_message_text: "#AAAAAA",
+  color_message_head_text: "#2E3B4A",
+  color_input: "#B4B4B4",
+  color_scroll: "#A6A6A6",
+  color_main: '#f57278',
+  color_sub: '#ff8888',
+  color_fail: '#707070',
+  color_middleBar: '#cbcbcb',
+  color_light: '#ffebec',
+  color_disable: '#afafaf',
 }
+
+const HTML_FONT_SIZE = 16;
+
+export const pxToRem = (px: number): number => {
+  return px / HTML_FONT_SIZE;
+};
