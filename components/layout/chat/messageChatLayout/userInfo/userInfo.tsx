7135e5973abe5b051d4ab666f64a54ef0da962bb
Jeon gyuhyun <jgh10070@naver.com>
2022-06-13 13:38:29 +0900

    [UPDATE] userInfo

--- components/layout/chat/messageChatLayout/userInfo/userInfo.tsx
+++ components/layout/chat/messageChatLayout/userInfo/userInfo.tsx
@@ -1,9 +1,11 @@
 import { useState, useEffect } from "react";
 import * as S from "./style";
+import { useSockets } from "@/../core/context/socket.context";
 import Banner from "@/../components/common/banner";
 
 const UserInfo = () => {
   const [isFriend, setIsFriend] = useState(false);
+  const { socket, messages, roomId, username } = useSockets();
 
   useEffect(() => {
     setIsFriend(true);
@@ -13,7 +15,7 @@ const UserInfo = () => {
     <S.Wrapper>
       <S.UserInfo>
         <S.UserBannerLayout>
-          <S.UserName>최예슬</S.UserName>
+          <S.UserName>{username}</S.UserName>
           <Banner isFriend={isFriend} />
         </S.UserBannerLayout>
         <S.UserRole>웹앱 UX 디자이너</S.UserRole>
